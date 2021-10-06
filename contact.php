a8e1c91486307bad97a5cfabc359b3437c461b10
amyjtech <79472580+amyjtech@users.noreply.github.com>
2021-10-06 14:32:48 -0700

    init

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php
+//TESTING (returns true every time)
+// Site key: 6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI
+// Secret key: 6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe
+
+$captcha = $_POST["captcha"]; //response data
+$secret = "SECRET-KEY-HERE"; //your recaptcha SECRET KEY
+
+//Recaptcha verification and JSON response decode
+$verify = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$captcha), true);
+
+//Value of json key "success"
+$success = $verify["success"];
+
+$name = stripslashes($_POST["name"]);
+$email = stripslashes($_POST["email"]);
+$subject = stripslashes($_POST["subject"]);
+$message = stripslashes($_POST["message"]);
+
+$headers = "From: " . $email . "\r\n" .
+    "Reply-To: " . $email . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+if ($success == false) {
+  //This user was not verified by recaptcha.
+  echo "Recaptcha Verification Failed";
+} else if ($success == true) {
+    //This user is verified by recaptcha
+    if (mail("EMAIL HERE", $subject, $Body, $headers)){
+      //send successful
+      echo "Recaptcha Success, Mail Sent Successfully";
+    }else{
+      //send failure
+        echo "Mailing Failed";
+      }
+}
+
+?>
